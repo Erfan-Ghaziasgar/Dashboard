cda5bd4420ede508d635f21758ceec05a80b0244
Erfan Pro97 <ghaziasgar.seyederfan@gmail.com>
2023-07-27 16:46:42 +0330

    Init Stramlit Dashboard

---
+++ src/run.py
@@ -0,0 +1,4 @@
+import streamlit as st
+# create a title for your app with emoji icon. text align to center.
+st.title("Streamlit App :sunglasses: ")
+st.header("Hi :wave: Welcome to Streamlit App")
